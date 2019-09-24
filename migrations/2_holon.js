c6678dd56c19e2dd631c258d9d4a8c50913825c4
Victor Hugo Ramos Fernandes de Santana <victor@janusproj.com>
2019-09-24 11:25:30 -0300

    main contract file separation in 4 contracts

--- migrations/2_holon.js
+++ migrations/2_holon.js
@@ -1,5 +1,11 @@
+const HolonStorage = artifacts.require("HolonStorage");
 const Holon = artifacts.require("Holon");
+const HolonPersona = artifacts.require("HolonPersona");
+const HolonValidator = artifacts.require("HolonValidator");
 
-module.exports = function(deployer) {
-  deployer.deploy(Holon);
+module.exports = async deployer => {
+  await deployer.deploy(HolonStorage);
+  await deployer.deploy(Holon, HolonStorage.address);
+  await deployer.deploy(HolonPersona);
+  await deployer.deploy(HolonValidator);
 };
