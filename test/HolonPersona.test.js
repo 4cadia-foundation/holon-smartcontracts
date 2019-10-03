7d5ae3043400e86a3a7de084db81c8f799616382
Yasmim Vieira <yasmim@4cadia.com>
2019-10-03 10:05:39 -0300

    Test in HolonPersona at function AddPersona and AddPersonaField

--- test/HolonPersona.test.js
+++ test/HolonPersona.test.js
@@ -1,15 +1,41 @@
-const HolonPersona = require('../contracts/HolonPersona.sol');
-const truffleAssertions = require('truffle-assertions');
+ const HolonStorage = artifacts.require('HolonStorage.sol');
+ const HolonPersona = artifacts.require('HolonPersona.sol');
+ const truffleAssertions = require('truffle-assertions');
 
-contract('HolonPersona', accounts => {
-    beforeEach(async () => {
-        this.contract = await HolonPersona.new(name, price, 0)
-        this.accountOwner = accounts[0];
-    });
+ contract('HolonPersona', (accounts) => {
+
+    let owner = accounts[0]
+    let personaAddress = accounts[1]
+    let personaAddress2 = accounts[2]
+
+    beforeEach('setup for each test', async () => {
+        holonStorage = await HolonStorage.deployed({from: owner})
+        holonPersona = await HolonPersona.deployed({from: owner})
+    })
+
     describe('Add Persona', () => {
-        it('should persona not exist',async () => {
-            const persona = await this.contract(name, price);
-            await truffleAssertions.passes(persona.name);
-        });
-    });
+
+        it('Deploying HolonStorage contract properly...', async () =>{
+            assert(HolonPersona.personaAddress != '');
+        })
+    
+        it('Persona not exist', async () => {
+            const isPersona = await holonStorage.isPersona(personaAddress)
+            assert.equal(isPersona, false)
+        })
+    
+        it('Persona add', async () => {
+            const addPersona = await holonPersona.addPersona('Yasmim', 0)
+            truffleAssertions.passes(addPersona);
+            
+        })
+    
+        it('Persona exist', async () => {
+            const addPersona = await holonPersona.addPersona('Yasmim', 0, {from: personaAddress} )
+            truffleAssertions.passes(addPersona);
+            const isPersona = await holonStorage.isPersona(personaAddress)
+            assert.equal(isPersona, true)
+    
+        })
+    })
  })
