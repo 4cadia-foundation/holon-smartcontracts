10e5bd6f71d844d38595bba92ffd50a6611bc700
Rodrigo Oliveira <rodrigo.soliveira2322@gmail.com>
2020-06-25 17:20:19 -0300

    configure truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,41 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+require('dotenv').config();
+
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
+        host: '127.0.0.1',
+        port: 8545,
+        network_id: '*',
+        gas: 8000000,
+        gasPrice: 10000000000
+    },
+    ropsten: {
+        provider: () => new HDWalletProvider(process.env.MNEUNOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+        network_id: 3,
+        gas: 3000000,
+        gasPrice: 10000000000
+    },
+    kovan: {
+        provider: () => new HDWalletProvider(process.env.MNEUNOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+        network_id: 42,
+        gas: 3000000,
+        gasPrice: 10000000000
+    },
+    rinkeby: {
+        provider: () => new HDWalletProvider(process.env.MNEUNOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+        network_id: 4,
+        gas: 7000000,
+        gasPrice: 10000000000
+    },
+    main: {
+        provider: () => new HDWalletProvider(process.env.MNEUNOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+        network_id: 1,
+        gas: 3000000,
+        gasPrice: 10000000000
     }
-  },
+},
   compilers: {
     solc: {
       version: "0.5.11",
